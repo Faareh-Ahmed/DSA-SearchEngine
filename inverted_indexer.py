90b8c1d6499588b1121f788e0dc808a673357cdf
Faareh-Ahmed <faarehahmed@gmail.com>
2023-12-14 00:30:30 +0500

    Updated the Barreling and added Rank in inverted index

--- inverted_indexer.py
+++ inverted_indexer.py
@@ -1,6 +1,7 @@
 import json
 import nltk
 import os
+from math import log
 
 nltk.download("punkt")
 
@@ -16,40 +17,69 @@ os.makedirs(output_folder, exist_ok=True)  # Create the folder if it doesn't exi
 
 # Load existing inverted index barrels if they exist
 barrels = {}
-for char in range(ord('a'), ord('z') + 1):
-    barrel_file = os.path.join(output_folder, f"inverted_index_{chr(char)}.json")
-    if os.path.exists(barrel_file):
-        with open(barrel_file, "r") as file:
-            barrels[chr(char)] = json.load(file)
+for char1 in range(ord('a'), ord('z') + 1):
+    for char2 in range(ord('a'), ord('z') + 1):
+        char = str(chr(char1) + chr(char2))
+        barrel_file = os.path.join(output_folder, f"inverted_index_{char}.json")
+        if os.path.exists(barrel_file):
+            with open(barrel_file, "r") as file:
+                barrels[char] = json.load(file)
+        else:
+            barrels[char] = {}
+
+# Include combinations where the second character is from '0' to '9'
+for char1 in range(ord('a'), ord('z') + 1):
+    for char2 in range(ord('0'), ord('9') + 1):
+        char = str(chr(char1) + chr(char2))
+        barrel_file = os.path.join(output_folder, f"inverted_index_{char}.json")
+        if os.path.exists(barrel_file):
+            with open(barrel_file, "r") as file:
+                barrels[char] = json.load(file)
+        else:
+            barrels[char] = {}
+
 
 for char in range(10):
     barrel_file = os.path.join(output_folder, f"inverted_index_{char}.json")
     if os.path.exists(barrel_file):
         with open(barrel_file, "r") as file:
             barrels[str(char)] = json.load(file)
+    else:
+        barrels[str(char)]={}
 
 barrel_file_other = os.path.join(output_folder, "inverted_index_other.json")
 if os.path.exists(barrel_file_other):
     with open(barrel_file_other, "r") as file:
         barrels['other'] = json.load(file)
+else:
+        barrels['other']={}
 
 # print(barrels)
 
 # Path to test file
 json_file_path = "C:\\Users\\user\\OneDrive\\Desktop\\3rd Semester\\DSA\\Project\\nela-gt-2022.json\\nela-gt-2022\\test_forward_index_files\\forward_index_0.json"
-
+count=0
 # Opening the file of forward index
 with open(json_file_path, "r") as file:
     data = json.load(file)
 
 for test_entry in data:
+    count+=1
+    print(count)
+    if(count>10000):
+        break
     doc_id = test_entry["doc_id"]
     stemmed_tokens = test_entry["stemmed_tokens"]
     frequency = test_entry["token_frequency"]
 
     for position, token in enumerate(stemmed_tokens, start=1):
         frequency = test_entry["token_frequency"][token]
+        position = test_entry["token_positions"].get(token, [])
 
+        # Calculate TF-IDF rank
+        tf = frequency / len(stemmed_tokens)
+        idf = log(len(stemmed_tokens) / (frequency + 1))  # Adding 1 to avoid division by zero
+        rank = tf * idf
         # Get the first character of the token
         first_char = str(token)[0].lower() if str(token) else None
 
@@ -59,29 +89,50 @@ for test_entry in data:
             # Check if the first_char is numeric and update the inverted index entry in the 'numeric' barrel
             if '0' <= first_char <= '9':
                 barrel = first_char
+
             elif 'a' <= first_char <= 'z':
-                barrel = first_char
+                # print(token)
+                if(len(token)<2):
+                    second_char=first_char
+                    char=str(first_char+second_char)
+                    barrel=char
+                else:
+                    second_char = str(token)[1].lower() if str(token) else None
+                    # Check if the second_char is not a lowercase letter or a digit
+                    if('0' <= second_char <= '9') or ('a' <= second_char <= 'z'):
+                        char=str(first_char+second_char)
+                        barrel = char
+                    else:
+                        barrel = 'other'
             else:
                 barrel = 'other'
 
 
         # Create the inverted index entry for the token if not present in the barrel
             if token not in barrels[barrel]:
-                barrels[barrel][token] = {"documents": [], "positions": {}, "frequency": {}}
+                barrels[barrel][token] ={}
 
             # Update the inverted index entry
-            if doc_id not in barrels[barrel][token]["documents"]:
-                barrels[barrel][token]["documents"].append(doc_id)
+            if doc_id not in barrels[barrel][token]:
+                word_details={
+                    "frequency":frequency,
+                    "position":position,
+                    "rank":rank
+                }
+                barrels[barrel][token][doc_id]=word_details
 
-            if doc_id not in barrels[barrel][token]["positions"]:
-                barrels[barrel][token]["positions"][doc_id] = [position]
-            else:
-                barrels[barrel][token]["positions"][doc_id].append(position)
+                
 
-            if doc_id not in barrels[barrel][token]["frequency"]:
-                barrels[barrel][token]["frequency"][doc_id] = frequency
+            # if doc_id not in barrels[barrel][token]["positions"]:
+            #     barrels[barrel][token]["positions"][doc_id] = [position]
+            # else:
+            #     barrels[barrel][token]["positions"][doc_id].append(position)
+
+            # if doc_id not in barrels[barrel][token]["frequency"]:
+            #     barrels[barrel][token]["frequency"][doc_id] = frequency
 
 # Save inverted index barrels to separate files
+print("Starting Writing to FIle\n")
 for char, inverted_index in barrels.items():
     inverted_index_file = os.path.join(output_folder, f"inverted_index_{char}.json")
     with open(inverted_index_file, "w") as file:
--- querytest.py
+++ querytest.py
@@ -15,17 +15,32 @@ stemmer = SnowballStemmer(language="english")
 def search_inverted_index(query_word):
     # Stem the query word
     stemmed_query_word = stemmer.stem(query_word)
+    print(stemmed_query_word)
 
     # Get the first character of the stemmed query word
     first_char = stemmed_query_word[0].lower() if stemmed_query_word else None
-
+    print(first_char)
+   # Ensure first_char is not None (empty or not a string)
     # Ensure first_char is not None (empty or not a string)
     if first_char is not None:
         # Check if the first_char is numeric and update the inverted index entry in the 'numeric' barrel
         if '0' <= first_char <= '9':
             barrel = first_char
+
         elif 'a' <= first_char <= 'z':
-            barrel = first_char
+            # print(token)
+            if(len(stemmed_query_word)<2):
+                second_char=first_char
+                char=str(first_char+second_char)
+                barrel=char
+            else:
+                second_char = str(stemmed_query_word)[1].lower() if str(stemmed_query_word) else None
+                # Check if the second_char is not a lowercase letter or a digit
+                if('0' <= second_char <= '9') or ('a' <= second_char <= 'z'):
+                    char=str(first_char+second_char)
+                    barrel = char
+                else:
+                    barrel = 'other'
         else:
             barrel = 'other'
 
@@ -41,7 +56,7 @@ def search_inverted_index(query_word):
         print(f"Inverted index file for '{query_word}' not found.")
         return
 
-
+    print("File Barrel Loaded Successfully")
      # Load the DocURL file
     try:
         with open(docurl_file_path, "r") as docurl_file:
@@ -52,31 +67,34 @@ def search_inverted_index(query_word):
 
     # Check if the stemmed query word is in the inverted index
     if stemmed_query_word in inverted_index:
-        # Get the documents, positions, and frequency information for the word
-        documents = inverted_index[stemmed_query_word]["documents"]
-        positions = inverted_index[stemmed_query_word]["positions"]
-        frequency = inverted_index[stemmed_query_word]["frequency"]
+     
+
+        documents=list(inverted_index[stemmed_query_word].keys())
 
         # print(documents)
-        # print(positions)
+        print("ooo\n")
+        rank=[]
+        for doc_id in documents:
+            # print(doc_id)
+            rank.append(inverted_index[stemmed_query_word][ doc_id ]["rank"])
+        # print(rank)
+
+        # Sort documents by rank
+        sorted_documents = sorted(documents, key=lambda doc_id: inverted_index[stemmed_query_word][doc_id]["rank"], reverse=True)
+
+        # Select the top 10 documents
+        top_documents = sorted_documents[:10]
+
         # Output the results
         print(f"Stemmed Query Word: {stemmed_query_word}")
-        print("Documents:")
-        for doc_id in documents:
-            print(f"  Document ID: {doc_id}")
+        print("Top 10 Documents:")
+        for doc_id in top_documents:
+            # print(f"  Document ID: {doc_id}")
 
-            # if str(doc_id) in positions:
-            #     print(f"    Positions: {positions[str(doc_id)]}")
-            # else:
-            #     print("    Positions: Not available")
-            # if str(doc_id) in frequency:
-            #     print(f"    Frequency: {frequency[str(doc_id)]}")
-            # else:
-            #     print("Frequency not available")
-            
             if str(doc_id) in docurls:
-                print(f"URL : {docurls[str(doc_id)]}")
+                print(f"  URL : {docurls[str(doc_id)]}")
             print("")
+
 
 
     else:
